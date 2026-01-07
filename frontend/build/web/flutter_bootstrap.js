eff312c8131e6d206f0ac7236f1a7b2325021058
sasanun <sasanun0129teaus@gmail.com>
2026-01-07 09:44:41 +0900

    広告機能ビルド

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1534554614"
+    serviceWorkerVersion: "1055646106"
   }
 });
